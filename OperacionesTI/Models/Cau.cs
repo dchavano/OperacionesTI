5ba0b91b381e7cff9fadd5e5cd5de1b7c0f16c8c
Chavano <Diego.Chavano@cencosud.com.ar>
2017-12-04 14:41:31 -0300

    Agregar archivos de proyecto.

---
+++ OperacionesTI/Models/Cau.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OperacionesTI.Models
+{
+    public class Cau
+    {
+        public int IdCau { get; set; }
+        public string Numero { get; set; }
+        public string Propietario { get; set; }
+        public int Grupo { get; set; }
+        public string Resumen { get; set; }
+        public DateTime Fecha { get; set; }
+        public int Estado { get; set; }
+        public int Region  { get; set; }
+        public int Tecnico { get; set; }
+
+    }
+}
