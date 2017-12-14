073964faf298f4146333f473798b44125e9fc3e2
Diego Chavaño <diegochavan@speedy.com.ar>
2017-12-14 17:01:59 -0300

    un

---
+++ OperacionesTI/Models/Roturas-UN.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OperacionesTI.Models
+{
+    public class Roturas_UN
+    {
+
+        public int IdUN { get; set; }
+        public string UnidadNegocio { get; set; }
+
+        //relacion a roturas
+    }
+}
